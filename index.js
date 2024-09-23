2d0f5f4115ab89c50ed4540b20d23db3e21a341b
leventtkarakaya <leventtkarakaya@gmail.com>
2024-09-23 22:50:41 +0300

    updated

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const db = require("./Config/db");
 dotenv.config();
 
 const app = express();
-
+/* Database */
 db();
-
+/* Cors */
 app.use(cors());
-
+/*  Body Parser */
 app.use(express.json({ extended: true, limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World! I am working fine");
 });
-
+/* Routers */
 const flight = require("./Routers/FlightRouter");
 const Image = require("./Routers/ImageRouter");
 const auth = require("./Routers/AuthRouter");
@@ -25,7 +25,7 @@ const auth = require("./Routers/AuthRouter");
 app.use("/api/v1/flight", flight);
 app.use("/api/v1/image", Image);
 app.use("/api/v1/auth", auth);
-
+/*  */
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Bir hata oluştu!" });
